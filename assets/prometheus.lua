27aff4205938d1cee99e4e099846aef8968deaed
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-07-30 08:17:31 +0200

    Create prometheus.lua

---
+++ assets/prometheus.lua
@@ -0,0 +1,19 @@
+{
+  "author": "Defold",
+  "description": "Lua obfuscation using Prometheus.",
+  "id": "prometheus",
+  "images": {
+    "hero": "",
+    "thumb": ""
+  },
+  "library_url": "https://github.com/defold/extension-prometheus/archive/refs/heads/master.zip",
+  "license": "MIT License",
+  "name": "Prometheus",
+  "platforms": [],
+  "project_url": "https://github.com/defold/extension-prometheus",
+  "tags": [
+    "Lua",
+    "Security"
+  ],
+  "website_url": "https://defold.com/manuals/application-security/#source-code-obfuscation"
+}
